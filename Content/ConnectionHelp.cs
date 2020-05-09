b7880d739df91540a78079f1719dd7be7b0f6572
rm0445530 <58572356+rm0445530@users.noreply.github.com>
2020-05-09 18:31:51 -0500

    test

---
+++ Content/ConnectionHelp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace WebApplication3.Content
+{
+    public class ConnectionHelp
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
